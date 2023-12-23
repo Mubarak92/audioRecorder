4f8e572ddfc1ac87b9b81ad67e81fb08c814507c
Mubarak-1998 <mubarak.alqnaam@gmail.com>
2023-12-23 13:29:35 +0300

    - Fix all bugs

--- app/src/main/java/com/example/audiorecorder/AudioRecorderFragment.kt
+++ app/src/main/java/com/example/audiorecorder/AudioRecorderFragment.kt
@@ -17,37 +17,45 @@ import android.widget.SeekBar
 import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.example.audiorecorder.databinding.FragmentAudioRecorderBinding
-import kotlinx.coroutines.launch
 import java.io.File
 import java.io.IOException
-private const val SEEK_BAR_UPDATE_INTERVAL = 100
 
-
+private const val LOG_TAG = "AudioRecordTest"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
-class AudioRecorderFragment : Fragment() {
+class AudioRecorderFragment : Fragment()  {
 
-    private val viewModel: AudioRecorderViewModel by viewModels()
+    private var recordingDuration = 0L
 
     private var fileName: String = ""
+
+    private var recorder: MediaRecorder? = null
+
+    private var player: MediaPlayer? = null
+
+    private var isSeekBarBeingTouched = false
+
+    var mStartRecording = true
+    var mStartPlaying = true
+
+    // Requesting permission to RECORD_AUDIO
+    private var permissionToRecordAccepted = false
 
     // Declare those two so we can destroy it into onDestroyView to prevent memory leaks
     private val binding get() = _binding!!
     private var _binding: FragmentAudioRecorderBinding? = null
 
 
+
     private val updateSeekBarTask = object : Runnable {
         override fun run() {
-            if (viewModel.player != null && viewModel.player!!.isPlaying) {
-                if (!viewModel.isSeekBarBeingTouched) {
-                    val currentPosition = viewModel.player!!.currentPosition
-                    binding.audioSeekbar.progress = currentPosition
+            if (recorder != null) {
+                if (!isSeekBarBeingTouched) {
+                    binding.audioSeekbar.progress = (System.currentTimeMillis() - recordingDuration).toInt()
                 }
                 // Update every 100 milliseconds
-                binding.audioSeekbar.postDelayed(this, SEEK_BAR_UPDATE_INTERVAL.toLong())
+                binding.audioSeekbar.postDelayed(this, 100)
             }
         }
     }
@@ -57,8 +65,7 @@ class AudioRecorderFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val fragmentMainPageBinding =
-            FragmentAudioRecorderBinding.inflate(inflater, container, false)
+        val fragmentMainPageBinding = FragmentAudioRecorderBinding.inflate(inflater, container, false)
         _binding = fragmentMainPageBinding
         return binding.root
     }
@@ -69,86 +76,243 @@ class AudioRecorderFragment : Fragment() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        viewModel.permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
             grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
         } else {
             false
         }
     }
 
+    private fun onRecord(start: Boolean) {
+        if (start) {
+            startRecording()
+            binding.recordTimer.isCountDown = true
+            binding.recordTimer.base = SystemClock.elapsedRealtime() + 60000
+            binding.recordTimer.start()
+        } else {
+            stopRecording()
+            binding.recordTimer.stop()
+        }
+    }
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                seekTo(binding.audioSeekbar.progress)
+                start()
+
+                setOnCompletionListener {
+                    stopPlaying()
+                    // Reset SeekBar progress to 0
+                    binding.audioSeekbar.progress = 0
+                    mStartPlaying = true  // Update mStartPlaying to true
+                    Log.d(LOG_TAG, "Playback complete. mStartPlaying set to true.")
+
+                    // Change the play button icon to play
+                    binding.playButton.setImageDrawable(
+                        ResourcesCompat.getDrawable(
+                            resources,
+                            R.drawable.ic_play,
+                            null
+                        )
+                    )
+                }
+
+                // Update seek bar progress
+                binding.audioSeekbar.max = duration
+                val updateRunnable = object : Runnable {
+                    override fun run() {
+                        if (player != null && player!!.isPlaying) {
+                            if (!isSeekBarBeingTouched) {
+                                binding.audioSeekbar.progress = currentPosition
+                            }
+                            if (currentPosition >= duration) {
+                                stopPlaying()
+                                binding.audioSeekbar.progress = 0
+                            } else {
+                                binding.audioSeekbar.postDelayed(this, 100)
+                            }
+                        }
+                    }
+                }
+                binding.audioSeekbar.post(updateRunnable)
+
+                // Change the play button icon to pause
+                binding.playButton.setImageDrawable(
+                    ResourcesCompat.getDrawable(
+                        resources,
+                        R.drawable.ic_pause,
+                        null
+                    )
+                )
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+        }
+    }
+
+    private fun updatePlayButtonIcon() {
+        binding.playButton.setImageDrawable(
+            ResourcesCompat.getDrawable(
+                resources,
+                if (mStartPlaying || (player != null && !player!!.isPlaying)) R.drawable.ic_play else R.drawable.ic_pause,
+                null
+            )
+        )
+    }
+
+    private fun stopPlaying() {
+        try {
+            player?.apply {
+                if (isPlaying) {
+                    pause()
+                }
+                // Do not reset and release, just pause
+            }
+        } catch (e: Exception) {
+            Log.e(LOG_TAG, "Error stopping playback", e)
+        } finally {
+            player = null
+            // Update the play button icon after stopping playback
+            updatePlayButtonIcon()
+        }
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            recordingDuration = System.currentTimeMillis()
+            binding.root.postDelayed({
+                if (binding.holdToRecord.isPressed) { // Check if the button is still being held
+                    stopRecording()
+                    binding.recordTimer.stop()
+                    Toast.makeText(
+                        requireContext(),
+                        "Maximum recording time reached (1 minute)",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }, 60000) // 60000 milliseconds = 1 minute
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+            start()
+
+            // Start recording duration timer
+            recordingDuration = System.currentTimeMillis()
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            reset()
+            release()
+        }
+        recorder = null
+
+        // Calculate the recording duration
+        val recordingDurationMillis = System.currentTimeMillis() - recordingDuration
+
+        // Check if the recording duration is less than 1 second
+        if (recordingDurationMillis < 1000) {
+            // Show a toast indicating that the recording is too short
+            Toast.makeText(
+                requireContext(),
+                "Recording is too short (less than 1 second)",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // Delete the audio file
+            deleteAudioFile()
+        } else {
+            // Continue with normal processing
+            binding.audioSeekbar.progress = 0
+            // Remove the update task when recording stops
+            binding.audioSeekbar.removeCallbacks(updateSeekBarTask)
+        }
+    }
+
+    private fun deleteAudioFile() {
+        binding.audioSeekbar.progress = 0
+        val file = File(fileName)
+        if (file.exists()) {
+            file.delete()
+            Toast.makeText(requireContext(), "Audio file deleted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "No audio file to delete", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        updatePlayButtonIcon()
 
-
-        lifecycleScope.launch {
-            viewModel.totalDuration.collect { duration ->
-                binding.audioSeekbar.max = duration
-            }
-        }
-
-        lifecycleScope.launch {
-            viewModel.currentPosition.collect { position ->
-                if (!binding.audioSeekbar.isPressed) {
-                    binding.audioSeekbar.progress = position
-                }
-            }
-        }
-
+        // Set a listener to handle SeekBar changes
         binding.audioSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
                 if (fromUser) {
-                    viewModel.seekToRecordingPosition(progress.toLong())
+                    player?.seekTo(progress)
                 }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                viewModel.isSeekBarBeingTouched = true
+                isSeekBarBeingTouched = true
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                viewModel.isSeekBarBeingTouched = false
+                isSeekBarBeingTouched = false
             }
         })
+
         binding.recordButton.setOnClickListener {
-            viewModel.onRecord(viewModel.mStartRecording)
-            binding.recording.visibility = when (viewModel.mStartRecording) {
+            onRecord(mStartRecording)
+            binding.recording.visibility = when (mStartRecording) {
                 true -> VISIBLE
                 false -> GONE
             }
-            viewModel.mStartRecording = !viewModel.mStartRecording
+            mStartRecording = !mStartRecording
         }
 
         binding.holdToRecord.setOnLongClickListener {
-            viewModel.onRecord(true)
+            onRecord(true)
             binding.recording.visibility = VISIBLE
+
             true // Consume the long click event
         }
 
         binding.holdToRecord.setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_UP) {
                 binding.holdToRecord.performClick()
-                viewModel.onRecord(false)
+                onRecord(false)
                 binding.recording.visibility = GONE
-            }
-            false
-        }
 
-
-        binding.audioSeekbar.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_MOVE) {
-                viewModel.isSeekBarBeingTouched = true
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                viewModel.isSeekBarBeingTouched = false
-                viewModel.seekToRecordingPosition(binding.audioSeekbar.progress.toLong())
             }
             false
         }
 
         binding.playButton.setOnClickListener {
-            viewModel.onPlay(viewModel.mStartPlaying)
-            binding.playing.visibility = when (viewModel.mStartPlaying) {
+            onPlay(mStartPlaying)
+            binding.playing.visibility = when (mStartPlaying) {
                 true -> {
                     binding.playButton.setImageDrawable(
                         ResourcesCompat.getDrawable(
@@ -159,7 +323,6 @@ class AudioRecorderFragment : Fragment() {
                     )
                     VISIBLE
                 }
-
                 false -> {
                     binding.playButton.setImageDrawable(
                         ResourcesCompat.getDrawable(
@@ -171,11 +334,11 @@ class AudioRecorderFragment : Fragment() {
                     GONE
                 }
             }
-            viewModel.mStartPlaying = !viewModel.mStartPlaying
+            mStartPlaying = !mStartPlaying
         }
 
         binding.deleteButton.setOnClickListener {
-            viewModel.deleteAudioFile()
+            deleteAudioFile()
         }
     }
 
@@ -183,10 +346,10 @@ class AudioRecorderFragment : Fragment() {
         super.onCreate(icicle)
 
         // Record to the external cache directory for visibility
-        fileName = "${context?.externalCacheDir?.absolutePath}/audiorecordtest.m4a"
-        viewModel.setAudioFileName(fileName)
+        fileName = "${context?.externalCacheDir?.absolutePath}/audiorecordtest.mp3"
+
         // Request microphone permission if not granted
-        if (!viewModel.permissionToRecordAccepted) {
+        if (!permissionToRecordAccepted) {
             requestPermissions(
                 arrayOf(android.Manifest.permission.RECORD_AUDIO),
                 REQUEST_RECORD_AUDIO_PERMISSION
@@ -196,9 +359,9 @@ class AudioRecorderFragment : Fragment() {
 
     override fun onStop() {
         super.onStop()
-        viewModel.recorder?.release()
-        viewModel.recorder = null
-        viewModel.player?.release()
-        viewModel.player = null
+        recorder?.release()
+        recorder = null
+        player?.release()
+        player = null
     }
 }
